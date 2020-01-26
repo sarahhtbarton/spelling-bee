4a08dea29b52464a8d00fbed87abbd64d25cd5da
Richee Wilson <contact@richeewilson.com>
2020-01-26 23:41:52 +0000

    work in progress

--- spell-bee2.py
+++ spell-bee2.py
@@ -1,4 +1,22 @@
-n = input("enter a number...")
-nl[] = n
-print([nl])
+user_input = ['']
 
+def ui_and_check(user_input):
+    while True:
+        try:
+            n = int(input("enter a number..."))
+        except ValueError:
+            print('not an integer')
+            continue
+        else:
+            if n >= 100 or n <= 0:
+                print('number is out of range')
+                continue
+            print(str(n)+' is an integer')
+            user_input[0]=n
+            break
+    # return user_input
+
+
+ui_and_check(user_input)
+
+print(user_input[0])
